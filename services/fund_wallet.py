3fffda736017fb425ecb7ea8fc74aceaf7f6c098
Paulerpro <nrpaulsilas@gmail.com>
2025-04-13 23:33:10 +0100

    balance reload and funding logic

--- services/fund_wallet.py
+++ services/fund_wallet.py
@@ -1,8 +1,10 @@
 from dotenv import load_dotenv
-
+from eth_account import Account
 from tools import Tools
+
 import logging
 import sys
+import random
 
 load_dotenv()
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
@@ -18,6 +20,43 @@ funder_key=crypto_tools.FUNDER_KEY
 
 funder_account = web3.eth.account.from_key(funder_key)
 
-# funding logic here
+private_keys=crypto_tools.fetch_accounts().get("private_keys", None)
+
+# Threshold below which wallet gets topped up (in wei)
+THRESHOLD = web3.to_wei(0.001, 'ether') # If a wallet has less than 0.001 ETH, it will be funded.
+TOP_UP_AMOUNT = web3.to_wei(0.005, 'ether') # top up 0.005 ETH
+
+GAS_LIMIT = 21000
+GAS_PRICE = web3.eth.gas_price # current price of gas from the blockchain
+
+
+def low_balance(balance):
+    if balance < THRESHOLD:
+        return True
+    return False
+
+
 def fund_wallet():
-    pass
+
+    sender_key = random.choice(private_keys)
+    tx_sender = Account.from_key(sender_key)
+
+    try:
+        tx_receiver = random.choice([w.address for w in wallets if w.address != tx_sender.address])
+    except:
+        tx_receiver = random.choice([w for w in wallets if w != tx_sender.address])
+
+    balance = web3.eth.get_balance(tx_sender)
+
+    data = {
+            "sender_key": sender_key,
+            "sender": tx_sender,
+            "receiver": tx_receiver
+        }
+
+    if low_balance(balance):
+        # go ahead with refill
+        pass
+    else:
+        # proceed to send transaction
+        pass
